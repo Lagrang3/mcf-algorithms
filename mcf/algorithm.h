7d9da97f53ba47d1266c4fe5171b0d0d7ae6e3dc
Lagrang3 <lagrang3@protonmail.com>
2024-07-17 08:14:02 +0100

    BFS search
    Signed-off-by: Lagrang3 <lagrang3@protonmail.com>

--- mcf/algorithm.h
+++ mcf/algorithm.h
@@ -3,6 +3,27 @@
 
 #include <mcf/network.h>
 
+/* Search any path from source to destination using Breadth First Search.
+ *
+ * input:
+ * @ctx: tal allocator,
+ * @graph: graph of the network,
+ * @source: source node,
+ * @destination: destination node,
+ * @capacity: an arc i is traversable if capacity[i]>0,
+ *
+ * output:
+ * @prev: prev[i] is the arc that leads to node i for an optimal solution, it
+ * @return: true if the destination node was reached.
+ *
+ * precondition:
+ * |capacity|=graph_max_num_arcs
+ * |prev|=graph_max_num_nodes
+ * */
+bool BFS_path(const tal_t *ctx, const struct graph *graph,
+	      const struct node source, const struct node destination,
+	      const s64 *capacity, struct arc *prev);
+
 /* Computes the distance from the source to every other node in the network
  * using Dijkstra's algorithm.
  *
